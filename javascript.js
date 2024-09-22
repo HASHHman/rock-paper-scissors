788b0e3500cbca03ebf4150cf9724f016ee44b99
HASHHman <SAHHH.ir@gmail.com>
2024-09-22 18:32:27 +0330

    Write Playround function

--- javascript.js
+++ javascript.js
@@ -20,3 +20,20 @@ function getComputerChoice() {
 function getUserChoice() {
     return window.prompt("Please input your action(\"rock\" \"paper\" \"scissors\"): ", "rock");
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice = "", computerChoice = "") {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if (humanChoice == "rock" && computerChoice == "paper" || humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "scissors" && computerChoice == "rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerChoice++;
+    } else {
+        console.log(`It's a draw! ${humanChoice} draw ${computerChoice}`)
+    }
+}
