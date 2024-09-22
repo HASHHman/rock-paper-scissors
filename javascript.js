bf31b7de79ca2d7042b9917ae70b286475aacd91
HASHHman <SAHHH.ir@gmail.com>
2024-09-22 17:59:00 +0330

    Write getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("H")
+function getComputerChoice() {
+    randomInt = Math.floor(Math.random() * 3);
+    switch (randomInt) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+        
+        default:
+            return "random integer isn't 0 or 1 or 2";
+            break;
+    }
+}
